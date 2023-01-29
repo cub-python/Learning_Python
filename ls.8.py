7685b7a931b48500543c74cd0fb487e1e254d240
jka <sk-west@bk.ru>
2023-01-29 13:55:31 +0300

    (if,elif,else-примеры)

---
+++ ls.8.py
@@ -0,0 +1,50 @@
+# Программа выводит по выбранной команде итог, choice - vybor
+help = '*', '/', "+", '-'
+comand = input('Пожалуйста введите команду: ')
+
+if comand == 'help':
+    print('help')
+elif comand == '*':
+    num1 = float(input('Введите 1-го множителя: '))
+    num2 = float(input('Введите 2-го множителя: '))
+    result = (num1 * num2)
+    print( num1,'*', num2, '=', result)
+
+elif comand == '/':
+    num1 = float(input('Введите делимое: '))
+    num2 = float(input('Введите делитель: '))
+    result = (num1 / num2)
+    print(num1, '/', num2, '=', result)
+
+elif comand == '+':
+    num1 = float(input('Введите 1-е слагаемое: '))
+    num2 = float(input('Введите 2-е слагаемое'))
+    result = (num1 + num2)
+    print('Сумма', num1, '+', num2, '=', result)
+
+
+elif comand == '-':
+    num1 = float(input('Введите уменьшаемое: '))
+    num2 = float(input('Введите вычитаемое: '))
+    result = (num1 - num2)
+    print('Разность', num1, '-', num2, '=', result)
+
+else:
+    print('Упсс, что-то нито,исправтье ошибку!')
+
+# Номер, месяц = период года
+
+monthNumber = int(input('Введите номер месяца: '))
+if monthNumber == 12 or monthNumber == 1 or monthNumber == 2:
+    print('Вы ввели цифру', str(monthNumber) + ', это зима.')
+elif monthNumber == 9 or monthNumber == 10 or monthNumber == 11:
+    print('Вы ввели цифру', str(monthNumber) + ',это осень.')
+
+elif monthNumber == 6 or monthNumber == 7 or monthNumber == 8:
+    print('Вы ввели цифру', str(monthNumber) + ',это лето.')
+
+elif monthNumber == 3or monthNumber == 4 or monthNumber == 5:
+    print('Вы ввели цифру', str(monthNumber) + ', это весна.')
+
+else:
+    print('Упсс, вы ввели неправильную цифру. ')
